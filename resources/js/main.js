ba963cc81c7b95c3a3e9c4753bab9a4ec441981f
DDeganoFrontaly <ddegano@frontaly.com>
2023-01-20 03:33:54 -0300

    Roberto Rocca v1

---
+++ resources/js/main.js
@@ -0,0 +1,22 @@
+
+import cookie from './cookie.js';
+import header from './header_sticky.js';
+import Swiper from './swiper.min.js';
+import swipersPersonalizados from './swipers_personalizados.js';
+import menu from './menu.js';
+import alert from './alert.js';
+import animation from './animation.js';
+import animationSchool from './animacion-red-de-escuelas-tecnicas.js';
+import video from './video.js';
+import faqs from './faqs.js';
+
+animation();
+animationSchool();
+header();
+alert();
+video();
+menu();
+cookie();
+swipersPersonalizados(Swiper);
+faqs();
+
